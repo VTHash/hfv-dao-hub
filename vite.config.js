53c1558cebe8867e66e0b72189f9bb94574c2420
VTHash <hfv@highfinanceventures.com>
2025-09-08 17:24:29 +0000

    Add netlify.toml redirects and vite full configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-export default {
-    optimizeDeps:{
-        include: ['@supabase/supabase-js']
-      }
-    }
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  base: '/', // critical for Netlify
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['@supabase/supabase-js']
+  }
+  // optional local dev niceties:
+  // server: { host: true, port: 5173 }
+})
